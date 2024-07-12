747e073a079c64f4f0acc988bcd9bd4d9f1aba6b
Isaac Sebaske <issebaske@gmail.com>
2024-07-12 16:04:46 -0600

    A5-2: Project Deliverable 7: jQuery Sticky Cart

--- js/app.js
+++ js/app.js
@@ -67,3 +67,17 @@ function updateQuantity() {
         });
     }
 }
+
+$(document).ready(()=>{
+    let sidebarOrigin = $("aside").offset();
+    $(window).scroll(()=>{
+        let scrollPosition = $('html').scrollTop();
+        if ( sidebarOrigin.top < scrollPosition){
+            $("aside").addClass("sticky");
+            $("aside").css("width", "17.9%");
+        } else {
+            $("aside").removeClass("sticky");
+            $("aside").css("width", "20%");
+        }
+    });
+})
